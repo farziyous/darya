87680ad0e4c92cd8e3d97b0aed6bee716dd11cc1
farziyous <farzinyaghoobi1388@gmail.com>
2026-09-22 12:27:15 +0330

    on god i hate seo1

--- darya-frontend/src/pages/products/Products.jsx
+++ darya-frontend/src/pages/products/Products.jsx
@@ -89,7 +89,7 @@ export const Products = ({ categories }) => {
 
     return (
         <>
-            <title>{activeCategory ? `${activeCategory.title} | کفش دریا` : search ? `نتایج جستجو برای ${search} | کفش دریا` : 'محصولات | خرید کفش و صندل | کفش دریا'}</title>
+            <title>{activeCategory ? `${activeCategory.title} | کفش دریا` : search ? `نتایج جستجو برای ${search} | کفش دریا` : 'محصولات | کفش و صندل | کفش دریا'}</title>
             <meta name="description" content={search ? `نتایج جستجو برای ${search} در فروشگاه کفش دریا` : `مشاهده و خرید ${activeCategory ? activeCategory.title : 'تمام محصولات'} کفش دریا`} />
 
             <Header />
